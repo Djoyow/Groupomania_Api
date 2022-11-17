fb93f021fcde6d948edbf0deb2342b0927380cd7
teddybengoto <teddybengoto@gmail.com>
2022-11-17 16:20:01 +0100

    replace http bay serveless

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const http = require('http');
 const app = require('./app');
 const bd = require('./bd/connect');
 
+const serverless = require('serverless-http')
+
 // Get normalize port
 
-const normalizePort = val => {
+/*const normalizePort = val => {
     const port = parseInt(val, 10);
 
     if (isNaN(port)) {
@@ -16,11 +18,11 @@ const normalizePort = val => {
     return false;
 };
 const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+app.set('port', port);*/
 
 // errorHandler
 
-const errorHandler = error => {
+/*const errorHandler = error => {
     if (error.syscall !== 'listen') {
         throw error;
     }
@@ -46,9 +48,10 @@ server.on('listening', () => {
     const address = server.address();
     const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
     console.log('Listening on ' + bind);
-});
+});*/
 
-server.listen(port);
+//server.listen(port);
+module.exports.handler = serverless(app);
 bd.conect();
 
 
